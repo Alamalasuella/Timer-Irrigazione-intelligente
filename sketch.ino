728797e8852ec57d54f8c0c513a57c5713a44f50
Farisca <133575682+Alamalasuella@users.noreply.github.com>
2023-07-01 01:58:18 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -18,6 +18,7 @@
 // La gestione del DHT22 e dell'ingresso analogico dovrà essere svolta 
 // senza usare le librerie specifiche.
 
+
 // Definisco bit e pin componenti:
 
 // DHT22
@@ -36,8 +37,8 @@
 #define BIT_PB (1 << PIN_PB)
 
 // Definizione dei livelli Minimo e Massimo di umidità del suolo
-#define MIN_HUMIDITY 30
-#define MAX_HUMIDITY 70
+#define MIN_HUMIDITY 30  
+#define MAX_HUMIDITY 70 
 
 // Definizione degli intervalli in secondi tra le letture dei sensori e l'intervallo giornaliero
 #define INTERVAL_DHT22 7200
@@ -60,73 +61,31 @@ bool readDigitalPin(uint8_t pin) {
 }
 
 // Contatori per gli intervalli tra le letture dei sensori e il contatore giornaliero
-volatile uint32_t counterDHT22 = INTERVAL_DHT22;
-volatile uint32_t counterPot = INTERVAL_POT;
 volatile uint32_t counterDay = INTERVAL_DAY;
+volatile uint32_t pos = 0;
+volatile uint16_t counterSecond = 0;
 
 // Variabili globali per memorizzare i valori delle misurazioni
 float temperature, humidity, soilHumidity;
-uint8_t currentHour = 0; // Ora corrente
-uint8_t currentMinute = 0; // Minuto corrente
-uint8_t irrigationTimes[16][2]; // Array degli orari di irrigazione
-uint8_t irrigationDurations[16]; // Array delle durate di irrigazione
-uint8_t irrigationCount = 0; // Contatore degli orari di irrigazione impostati
-
-
-// Funzione per impostare gli orari di irrigazione
-void setIrrigationTimes() {
-  Serial.println("Imposta gli orari di irrigazione:");
-  // Chiedi all'utente di inserire gli orari di irrigazione
-  for (uint8_t irrigationCount = 0; irrigationCount < 16; i++) {
-    Serial.print("Inserisci l'ora (0-23) per l'irrigazione ");
-    Serial.print(irrigationCount + 1);
-    Serial.print(": ");
-    while (!Serial.available()); // Aspetta che l'utente inserisca un valore
-    uint8_t hour = Serial.parseInt(); // Leggi l'ora inserita dall'utente
-    Serial.print("Inserisci il minuto (0-59) per l'irrigazione ");
-    Serial.print(irrigationCount + 1);
-    Serial.print(": ");
-    while (!Serial.available()); // Aspetta che l'utente inserisca un valore
-    uint8_t minute = Serial.parseInt(); // Leggi il minuto inserito dall'utente
-    // Controllo se l'ora e il minuto inseriti sono validi
-    if (hour < 24 && minute < 60) {
-      irrigationTimes[irrigationCount][0] = hour;
-      irrigationTimes[irrigationCount][1] = minute;
-      irrigationCount++;
-      Serial.println("Orario impostato correttamente!");
-    } else {
-      Serial.println("Orario non valido! Riprova.");
-    }
-  }
-}
-
-// Funzione per impostare le durate di irrigazione
-void setIrrigationDurations() {
-  Serial.println("Imposta le durate di irrigazione:");
-  // Chiedi all'utente di inserire le durate di irrigazione
-  for (uint8_t i = 0; i < irrigationCount; i++) {
-    Serial.print("Inserisci la durata (minuti) per l'irrigazione ");
-    Serial.print(i + 1);
-    Serial.print(": ");
-    while (!Serial.available()); // Aspetta che l'utente inserisca un valore
-    irrigationDurations[i] = Serial.parseInt(); // Leggi la durata inserita dall'utente
-    Serial.println("Durata impostata correttamente!");
-  }
-}
+// Inizializzo Ora e Minuti correnti, il Contatore degli orari di irrigazione impostati,
+// gli Array degli orari di irrigazione e gli Array delle durate di irrigazione
+uint8_t currentHour, currentMinute, irrigationCount;
+uint32_t irrigationTimes[16], irrigationDurations[16];
+uint32_t iTimes[16], iDurations[16];
 
 
 void setup() {
+  Serial.begin(9600); // Avvio la comunicazione seriale
   TCCR1A = 0; // Azzero il registro di controllo A del timer 1
   OCR1A = 15624; // Registro di confronto per l'interrupt ogni secondo
   TCCR1B |= (1 << WGM12); // Abilito la modalità di confronto CTC
   TCCR1B = (TCCR1B & 0xF8) | 0x05; // Imposto il prescaler a 1024
-  // TCCR1B = (TCCR1B & 0xF8) | 0x01; // Simulazione giornata con prescaler a 1
   TIMSK1 |= (1 << OCIE1A); // Abilito l'interrupt di confronto A del timer 1
-  // Inizializza la comunicazione seriale
-  Serial.begin(9600);  
-  while (!Serial);  // Aspetta che la comunicazione seriale sia disponibile
-  setIrrigationTimes();  // Imposta gli orari di irrigazione
-  setIrrigationDurations();  // Imposta le durate di irrigazione
+  PCICR |= (1 << PCIE2); // Attivo il PIN change Interrupt per i PIN dal 23 al 16
+  PCMSK2 |= (1 << PCINT22); // Attivo il PIN change Interrupt solo per il PIN 22
+  setPinMode(PD6, INPUT_PULLUP); // Imposto il PushButton come INT_PULLUP
+  SerialSetUp();
+  Serial.println("Orari di controllo aggiornati con successo!");
 }
 
 void loop() {
@@ -136,36 +95,30 @@ void loop() {
 // Funzione per leggere il sensore DHT22
 void readDHT22(uint8_t pin) {
   uint8_t data[5] = {0, 0, 0, 0, 0};
-
   // Segnale di avvio
   setPinMode(BIT_DHT22, OUTPUT);
   setDigitalLevel(BIT_DHT22, LOW);
   delayMicroseconds(1000);
   setDigitalLevel(BIT_DHT22, HIGH);
   delayMicroseconds(40);
-
   // Segnale di risposta
   setPinMode(BIT_DHT22, INPUT);
   while (readDigitalPin(BIT_DHT22));
   while (!readDigitalPin(BIT_DHT22));
   while (readDigitalPin(BIT_DHT22));
-
   // Lettura dei 40 bit disabilitando gli interrupt
   cli();
   for (uint8_t i = 0; i < 5; i++) {
     for (uint8_t j = 0; j < 8; j++) {
       while (!readDigitalPin(BIT_DHT22));
       delayMicroseconds(50);
-
       data[i] = (data[i] << 1);
       if (readDigitalPin(BIT_DHT22))
         data[i] |= 0x01;
-
       while (readDigitalPin(BIT_DHT22));
     }
   }
   sei();
-
   // Verifica del checksum e salvataggio dei valori nelle variabili globali
   if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
     humidity = ((data[0] << 8) | data[1]) / 10.0;
@@ -176,6 +129,7 @@ void readDHT22(uint8_t pin) {
   }
 }
 
+
 // Funzione per leggere il potenziometro
 void readPotentiometer(uint8_t pin) {
   ADMUX = (pin & 0xF8) | (pin & 0x07);
@@ -184,18 +138,50 @@ void readPotentiometer(uint8_t pin) {
   soilHumidity = ADC * (100.0 / 1023.0);
 }
 
+
+// Funzione per richiamare l'Interrupt per il TIMER1 sulla comparazione con OCR1A
 ISR(TIMER1_COMPA_vect) {
-  if (--counterDHT22 == 0) {
-    counterDHT22 = INTERVAL_DHT22;
-    readDHT22(PIN_DHT22);
+  if (--irrigationTimes[pos] == 0) {
+    irrigationTimes[pos] = iTimes[pos];
+    irrigationTimes[pos+1] -= irrigationTimes[pos];
+    pos += 1;
+    readDHT22(PIN_DHT22); // Leggo il DHT22
+    readPotentiometer(PIN_POT); // Leggo il Potenziometro
+    if (soilHumidity < MIN_HUMIDITY) { // Controllo il livello di umidità
+      setDigitalLevel(BIT_RELAY, HIGH); // Attiva il relay per l'irrigazione per la durata necessaria
+    }
   }
-
-  if (--counterPot == 0) {
-    counterPot = INTERVAL_POT;
-    readPotentiometer(PIN_POT);
-  }
-
   if (--counterDay == 0) {
     counterDay = INTERVAL_DAY;
+    pos = 0;
+  }
+}
+
+
+// Funzione per gestire la pressione del PushButton
+ISR(PCINT2_vect){
+  if (readDigitalPin(PD6) == false && counterSecond > 0) {
+    Serial.println("Pulsante premuto!");
+    Serial.println("Avvio Ciclo di Controllo...");
+    counterSecond = 0;
+  }
+}
+
+void SerialSetUp(){
+  Serial.println("Programma una Schedule Orari di Controllo :");
+  Serial.println("(Inserire un numero > 24 per terminare)");
+  for(uint32_t i = 0; i < 16; i++){
+    while (Serial.available() == 0);  // Se la comunicazione Seriale non è disponibile aspetta
+    irrigationTimes[i] = Serial.parseInt();
+    if (irrigationTimes[i] <= 24) {
+      irrigationCount = i;
+      iTimes[i] = irrigationTimes[i];
+      Serial.println("Orario inserito: ");
+      Serial.println(irrigationTimes[i]);
+      while (Serial.available() == 1);  // Aspetto che la linea seriale sia nuovamente bassa 
+    }
+    else{
+      break;
+    }
   }
 }
